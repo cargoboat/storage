583b3e134629d0aadf649bc8fb705870616d74ef
Kevin <862860000@qq.com>
2019-02-19 22:28:18 +0800

    add get value by prefix
    添加根据前缀获取所有value

--- storageer.go
+++ storageer.go
@@ -5,7 +5,9 @@ type Storageer interface {
 	Get(key string) (value string, err error)
 	Set(key string, value string) error
 	GetAllKeys() (keys []string, err error)
+	GetAllKeysByPrefix(prefix string) (keys []string, err error)
 	GetAll() (values map[string]string, err error)
+	GetAllByPrefix(prefix string) (values map[string]string, err error)
 	Delete(key string) error
 	Close() error
 }
